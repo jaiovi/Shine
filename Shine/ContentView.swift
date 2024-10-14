3329b437db9afd676bf299a001294ddc1b99807a
jaiovi <sebastianjo2011@gmail.com>
2024-10-14 10:10:16 +0200

    Initial Commit

---
+++ Shine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shine
+//
+//  Created by Jesus Sebastian Jaime Oviedo on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shine/ShineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShineApp.swift
+//  Shine
+//
+//  Created by Jesus Sebastian Jaime Oviedo on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ShineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
